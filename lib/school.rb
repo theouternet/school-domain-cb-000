9a76ead7b0b09d4d07756bab40561edd275c020c
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-03 22:09:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+end
+
+def roster
+  @roster
+end
+
+def add_student(name, grade)
+
+  @roster[grade] = [name]
+end
+
+end
